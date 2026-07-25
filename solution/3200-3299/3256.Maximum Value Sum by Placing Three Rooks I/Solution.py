3451fedb9cb40470a6246e546ef172fd09f2f824
Raj Giri <rg894710@gmail.com>
2026-07-26 00:39:11 +0530

    Refactor maximumValueSum method for clarity and efficiency

--- solution/3200-3299/3256.Maximum Value Sum by Placing Three Rooks I/Solution.py
+++ solution/3200-3299/3256.Maximum Value Sum by Placing Three Rooks I/Solution.py
@@ -1,29 +1,33 @@
 class Solution:
-    def maximumValueSum(self, board: List[List[int]]) -> int:
+    def maximumValueSum(self, A: List[List[int]]) -> int:
+        m, n = len(A), len(A[0])
+        ans = float("-inf")
+        max_val = [[float("-inf")] * 3 for _ in range(m)]
+        max_cols = [[-1] * 3 for _ in range(m)]
 
-        def top3(arr):
-            return sorted(range(len(arr)),key = lambda i:-arr[i])
+        for i in range(m):
+            cols = [(A[i][j], j) for j in range(n)]
+            cols.sort(reverse=True) 
 
-        
-        mx = -100000000000
-        seen = []
-        tops = []
-        for row in board:
-            tops.append(top3(row))
+            for k in range(min(3, n)):
+                max_val[i][k] = cols[k][0]
+                max_cols[i][k] = cols[k][1]
 
-        def recur(i,cur):
-            nonlocal mx
-            if len(seen) == 3:
-                mx = max(mx,cur)
-                return
-            if i==len(board):
-                return
-            recur(i+1,cur)
-            for top in tops[i]:
-                if top not in seen:
-                    seen.append(top)
-                    recur(i+1,cur+board[i][top])
-                    seen.pop()
-        
-        recur(0,0)
-        return mx
+        for r1 in range(m):
+            for r2 in range(r1 + 1, m):
+                for r3 in range(r2 + 1, m):
+                    for i in range(3):
+                        for j in range(3):
+                            if max_cols[r2][j] == max_cols[r1][i]:
+                                continue
+                            for k in range(3):
+                                if (max_cols[r3][k] == max_cols[r1][i] or
+                                    max_cols[r3][k] == max_cols[r2][j]):
+                                    continue
+
+                                current_sum = (max_val[r1][i] +
+                                               max_val[r2][j] +
+                                               max_val[r3][k])
+                                ans = max(ans, current_sum)
+
+        return ans
